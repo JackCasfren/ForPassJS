d4bb89c3f81925c6abe92b29213e0c3ae811184b
elian.casal <elian.casal@gmail.com>
2024-09-13 20:33:38 +0000

    triying diferent ways to get live preview

---
+++ .idx/dev.nix
@@ -0,0 +1,21 @@
+{ pkgs, ... }: {
+  channel = "stable-23.11";
+  packages = [
+    pkgs.nodejs_20
+    pkgs.python3
+    pkgs.nodePackages.live-server
+  ];
+  env = {};
+  idx = {
+    extensions = [];
+    previews = {
+      enable = true;
+      previews = {
+        web = {
+          command = ["live-server" "--port=$PORT" "--host=0.0.0.0" "--no-browser"];
+          manager = "web";
+        };
+      };
+    };
+  };
+}
