9d462dd40cb188fecbb0720960365b4be9d9ffdc
elian.casal <elian.casal@gmail.com>
2024-10-07 20:25:49 +0000

    making mistakes folder and realising ts is compiled

--- web/DEV/scripts/script.js
+++ web/DEV/scripts/script.js
@@ -3,14 +3,3 @@
 
 console.log("ts beeing wred");
 
-const dynSubBtn = document.getElementById("dynSubBtn");
-        const textBox = document.getElementById("textBox") as HTMLInputElement;
-        const resultDiv = document.getElementById("result");
-
-        dynSubBtn?.addEventListener("click", () => {
-            alert("test")
-            const textValue = textBox.value;
-            if (resultDiv && textValue) {
-                resultDiv.innerText = `You entered: ${textValue}`;
-            }
-});
