0e0d040a3533f543de038c64737085b86a247661
elian.casal <elian.casal@gmail.com>
2024-10-07 20:59:14 +0000

    starting to pick up steam. i made a stupid button. out of analisys paralisys.

--- web/DEV/scripts/script.js
+++ web/DEV/scripts/script.js
@@ -1,5 +1,20 @@
 //*Interacting with HTML
 
+// add value to the Dynamic values list
+function addDynVal() {
+//does it work?
+    console.log("addDynVal called");
+//getting the input value
+    let inputValue = document.getElementById("inputTextDyn").value;
+    console.log("inputValue:" + inputValue)
+//geting where the result will land
+    let  resultDiv = document.getElementById("result");
+//creating the new html code for the new variable.
+    const button = document.createElement("button");
 
-console.log("ts beeing wred");
+    button.textContent = inputValue;
+    button.id = inputValue;
+    button.className = "dynValBtn";
 
+    resultDiv.appendChild(button);
+}
