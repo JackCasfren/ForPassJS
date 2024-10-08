98d90baf71e96cd6297748edf6972305b96ee241
elian.casal <elian.casal@gmail.com>
2024-10-08 18:45:00 +0000

    fixed small spacing issue and the main issue Alquemar reported on INT variables not being reportable

--- web/DEV/scripts/script.js
+++ web/DEV/scripts/script.js
@@ -3,10 +3,12 @@
 function addDynVal() {
     console.log("addDynVal called");
 
-    // Getting the input value
-    let inputValue = document.getElementById("inputTextDyn").value.trim();
-    console.log("inputValue:", inputValue);
+    //! Getting the input value
+    //let inputValue = document.getElementById("inputTextDyn").value.trim();
+    let inputValue = String(document.getElementById("inputTextDyn").value.trim());
 
+    console.log("inputValue:", inputValue);
+    
     // Check if the input value is empty
     if (!inputValue) {
         console.log("Input is empty. Button will not be created.");
@@ -15,17 +17,29 @@ function addDynVal() {
 
     // Get the container where the result will land
     let resultDiv = document.getElementById("dynValList");
-
+    
     // Check if a button with the same ID already exists within resultDiv
-    if (resultDiv.querySelector(`#${inputValue}`)) {
+    //! for some reason this query selector does something to the value that it doesnt allow numbers
+    //! We will burn this code when we get to it.    
+    //if (resultDiv.querySelector(`#${inputValue.toString()}`)) {
+    /*
+       if (resultDiv.querySelector(`#${inputValue}`)) {
         console.log("Button with the same ID already exists inside the result div.");
         return;
     }
-
+    */
+    if (resultDiv.querySelector(`#ID-${inputValue}`)) {
+        console.log("Button with the same value already exists inside the result div.");
+        return;
+    }
+    
+    console.log("inputValue:", inputValue)
     // Creating the new HTML button
     const button = document.createElement("button");
     button.textContent = inputValue;
-    button.id = inputValue; // Avoid using non-unique IDs
+    
+    // "item-" needs to be there, becasue other wise query selector does like it.
+    button.id = "ID-" + inputValue; // Avoid using non-unique IDs
     button.className = "dynValBtn"; // added this to be able to consult if the val is Dyn or Base
 
     // Append the new button to the resultDiv
@@ -73,4 +87,7 @@ function addBaseVal() {
 
     console.log("Button added successfully.");
 }
+
+
+
 
